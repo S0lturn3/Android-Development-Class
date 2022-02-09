3d0d00c4e92b0be9fd84b61fbb72a774f046b608
Erick Carvalho <erickzcarvalho25@outlook.com>
2022-02-08 22:03:04 -0300

    Add files via upload

--- untitled/src/main/kotlin/Main.kt
+++ untitled/src/main/kotlin/Main.kt
@@ -3,27 +3,86 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 
+    //  Utilizando Int
+    /*
     println("Digite o primeiro valor:")
-    val value1: Int = Integer.valueOf(readLine())
+    val value1I: Int = Integer.valueOf(readLine())
     println("Digite o segundo valor:")
-    val value2: Int = Integer.valueOf(readLine())
+    val value2I: Int = Integer.valueOf(readLine())
 
-    val result = ( value1 + value2 ) / 2
+    val resultI = ( value1I + value2I ) / 2
 
-    print("O valor da média é: $result")
+    print("O valor da média é: $resultI")
 
-
+    //  Utilizando Float
     println("Digite o primeiro valor:")
-    val value3: Float = reader.nextFloat()
+    val value1F: Float = reader.nextFloat()
     println("Digite o segundo valor:")
-    val value4: Float = reader.nextFloat()
+    val value2F: Float = reader.nextFloat()
 
-    val resultF = ( value3 + value4 ) / 2
+    val resultF = ( value1F + value2F ) / 2
 
     print("O valor da média é: $resultF")
+    */
 
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html
+    //  Utilizando Double
+    /*println("Digite a quantidade de faltas do aluno: ")
+    val faltas: Double = reader.nextDouble()
+    val aulas: Int = 33;
+    val presenca: Double = (25.0 / 100.0) * aulas
+
+    if (faltas <= presenca) {
+
+        println("Digite a primeira nota: ")
+        val value1D: Double = reader.nextDouble()
+        println("Digite a segunda nota: ")
+        val value2D: Double = reader.nextDouble()
+
+        val resultD = ( value1D + value2D ) / 2
+
+        if (resultD >= 9) {
+            println("A nota do aluno foi 'A' e sua média foi $resultD. O aluno está aprovado!")
+        }
+        else if (resultD >= 7.5) {
+            println("A nota do aluno foi 'B' e sua média foi $resultD. O aluno está aprovado!")
+        }
+        else if (resultD >= 6) {
+            println("A nota do aluno foi 'C' e sua média foi $resultD. O aluno está aprovado!")
+        }
+        else if (resultD < 6) {
+            println("A nota do aluno foi 'D' e sua média foi $resultD. O aluno está reprovado!")
+        }
+    } else {
+        println("A nota do aluno foi 'FF'. O aluno está reprovado por Faltas!")
+    }
+    */
+
+
+    //  Utilizando Long
+    /*
+    println("Digite o primeiro valor:")
+    val value1L: Long = reader.nextLong()
+    println("Digite o segundo valor:")
+    val value2L: Long = reader.nextLong()
+
+    val resultL = ( value1L + value2L ) / 2
+
+    print("O valor da média é: $resultL")
+     */
+
+
+
+    for (M in 0..59) {
+        for (S in 0..59) {
+            if (S < 59) {
+                //S += 1
+            } else {
+                //M += 1
+                //S = 0
+            }
+        }
+    }
+
 }
 
