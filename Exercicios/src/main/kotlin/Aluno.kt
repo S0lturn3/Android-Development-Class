714538fb5927e860476079631782018318e65beb
Erick Carvalho <erickzcarvalho25@outlook.com>
2022-02-16 21:40:03 -0300

    Modificação da Classe aluno
    Modificação feita apenas para organização de código

--- Exercicios/src/main/kotlin/Aluno.kt
+++ Exercicios/src/main/kotlin/Aluno.kt
@@ -1,19 +1,21 @@
 
 public class Aluno(n1: Float, n2: Float) {
     private val result : Float = (n1 + n2) / 2
-    private var performance: String = ""
-
-    private val presentation: String = "\nO desempenho do aluno foi: "
-    private val sentence: String = "| Média final: %.2f \n\n\n".format(result)  //  ".format(val) usado em conjunto com "%.2f" arredonda o
-                                                                                // valor com pontos flutuantes para apenas duas casas decimais
+    private var gradeLetter: Char = 'A'
 
     public fun messageSelection(): String {
-        performance = if(result >= 9){ "$presentation\n 'A' $sentence" }
-                      else if(result >= 7.5) { "$presentation\n 'A' $sentence" }
-                      else if(result >= 6) { "$presentation\n 'A' $sentence" }
-                      else { "$presentation\n 'D' $sentence" }
+        gradeLetter = if (result >= 7.5) {
+            'B'
+        } else if (result >= 6) {
+            'C'
+        } else {
+            'D'
+        }
 
-        return performance
+        return ("\n-= DESEMPENHO DO ALUNO =- \n" +
+                "$gradeLetter | Média final: %.2f \n\n\n").format(result)
+        //  ".format(val) usado em conjunto com "%.2f" arredonda o valor com pontos flutuantes
+        //  para apenas duas casas decimais
     }
 }
 
