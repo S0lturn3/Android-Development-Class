8f10194c0babac66e8eefc94acc31353b716ac21
Erick Carvalho <erickzcarvalho25@outlook.com>
2022-02-16 22:00:56 -0300

    Update Aluno.kt

--- Exercicios/src/main/kotlin/Aluno.kt
+++ Exercicios/src/main/kotlin/Aluno.kt
@@ -1,6 +1,12 @@
-
+/**
+ * ## Classe relacionada ao Aluno
+ *
+ *  - Contém a função de `Cálculo de Desempenho Escolar` (possivelmente no futuro tendo outras funções);
+ *  - Atributos de `Notas` e `Média` e futuramente possuirá outros atributos, como "Nome", "Idade", "E-mail", etc.
+ *
+ * */
 public class Aluno(n1: Float, n2: Float) {
-    private val result : Float = (n1 + n2) / 2
+    private val result: Float = (n1 + n2) / 2
     private var gradeLetter: Char = 'A'
 
     public fun messageSelection(): String {
@@ -10,8 +16,7 @@ public class Aluno(n1: Float, n2: Float) {
 
         return ("\n-= DESEMPENHO DO ALUNO =- \n" +
                 "$gradeLetter | Média final: %.2f \n\n\n").format(result)
-        //  ".format(val) usado em conjunto com "%.2f" arredonda o valor com pontos flutuantes
+        //  ".format(variável) usado em conjunto com "%.2f" arredonda o valor com pontos flutuantes
         //  para apenas duas casas decimais
     }
 }
-
